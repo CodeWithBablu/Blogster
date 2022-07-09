d37097f3d653703a0be1bd39b1ae7332bf6f5c18
MrNoobCobra <hashxmash@pop-os.localdomain>
2022-07-10 00:33:48 +0530

    Version 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      animation: {
+        blob: "bolb 7s infinite",
+      },
+
+      keyframes:{
+        bolb: {
+          "0%":{
+            transform: "scale(1) translate(0px,0px)",
+          },
+          "33%":{
+            transform: "scale(1.05) translate(20px,-20px)",
+          },
+          "66%":{
+            transform: "scale(0.95) translate(-20px,20px)",
+          },
+          "100%":{
+            transform: "scale(1) translate(0px,0px)",
+          }
+        }
+      },
+
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
